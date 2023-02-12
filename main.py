b21a57ed1e08088a479337dfaf32c3234aa7dcc8
ruodeee <rudybpyt@gmail.com>
2023-02-12 18:27:32 +0000

    first commit

--- main.py
+++ main.py
@@ -11,15 +11,14 @@ for root, dirs, files in os.walk(rootdir):
                 with open(fr"{rootdir}\{dir}\{subdir}\setup.bat", "w") as f:
                     subdir2 = subdir.replace(" ","-")
                     l1 = f"gh repo create {subdir2} --private\n"
-                    l2 = f'echo "# {subdir2}" >> README.md \n'
-                    l3 = "git init\n"
-                    l4 = "git add --all :/\n"
-                    l5 = 'git commit -m "first commit"\n'
-                    l6 = "git branch -M main\n"
-                    l7 = "git remote remove origin\n"
-                    l8 = f"git remote add origin https://github.com/ruodeee/{subdir2}.git\n"
-                    l9 = "git push -u origin main"
-                    f.writelines([l1, l2, l3, l4, l5, l6, l7, l8, l9])
+                    l2 = "git init\n"
+                    l3 = "git add --all :/\n"
+                    l4 = 'git commit -m "first commit"\n'
+                    l5 = "git branch -M main\n"
+                    l6 = "git remote remove origin\n"
+                    l7 = f"git remote add origin https://github.com/ruodeee/{subdir2}.git\n"
+                    l8 = "git push -u origin main"
+                    f.writelines([l1, l2, l3, l4, l5, l6, l7, l8])
                 f.close()
                 with open(fr"{rootdir}\{dir}\{subdir}\README.md", "w") as f:
                     l1 = f"# {subdir}\n"
